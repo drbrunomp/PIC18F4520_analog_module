df62b93cfad28df26c838a52ee1a2a3c7152131f
drbrunomp <drbrunomp@gmail.com>
2024-07-24 14:18:08 -0300

    Modulo Analogico

---
+++ Leitura_AN1_VOLTS/Reading_AN1_VOLTS.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Reading_AN1_VOLTS.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Reading_AN1_VOLTS.X.production.hex
